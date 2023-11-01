ea0774a57af4fb2e801b5ad69021d63a3283803f
Ga Wu <wuga214@gmail.com>
2023-11-01 11:54:24 -0300

    bash update

--- experiments/debug.py
+++ experiments/debug.py
@@ -2,4 +2,4 @@ import numpy as np
 import torch
 
 def data_debugging(explainer, dataloader, seed=1, gpu=False):
-    
+    pass
--- explainability_bash.sh
+++ explainability_bash.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-python explainability.py -data CIFAR10 -n_classes 10 -network ResNet --gpu
+
+for i in {1..30}
+do  
+    python explainability.py -data CIFAR10 -n_classes 10 -network ResNet -explainer YADEA --gpu -seed $i
+    python explainability.py -data CIFAR10 -n_classes 10 -network ResNet -explainer RPS --gpu -seed $i
+    python explainability.py -data CIFAR10 -n_classes 10 -network ResNet -explainer IF --gpu -seed $i
+    python explainability.py -data CIFAR10 -n_classes 10 -network ResNet -explainer TracIn --gpu -seed $i
+done
+
+
+for i in {1..30}
+do  
+    python explainability.py -data OCEA -n_classes 5 -network ResNet -explainer YADEA --gpu -seed $i
+    python explainability.py -data OCEA -n_classes 5 -network ResNet -explainer RPS --gpu -seed $i
+    python explainability.py -data OCEA -n_classes 5 -network ResNet -explainer IF --gpu -seed $i
+    python explainability.py -data OCEA -n_classes 5 -network ResNet -explainer TracIn --gpu -seed $i
+done
