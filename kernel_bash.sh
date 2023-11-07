353f8f610267e69bdfe937d318e75fc8b7fdc99a
wuga <wuga214@gmail.com>
2023-11-07 10:52:38 -0500

    intermediate result push

--- kernel_bash.sh
+++ kernel_bash.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 
-for i in {1..30}
+for i in {1..15}
 do  
     python kernel.py -data CIFAR10 -n_classes 10 -network ResNet --gpu -seed $i -kernel RBF
     python kernel.py -data CIFAR10 -n_classes 10 -network ResNet --gpu -seed $i -kernel IMQ
     python kernel.py -data CIFAR10 -n_classes 10 -network ResNet --gpu -seed $i -kernel Linear
+    python kernel.py -data CIFAR10 -n_classes 10 -network ResNet --gpu -seed $i -kernel RBF --scale
+    python kernel.py -data CIFAR10 -n_classes 10 -network ResNet --gpu -seed $i -kernel IMQ --scale
+    python kernel.py -data CIFAR10 -n_classes 10 -network ResNet --gpu -seed $i -kernel Linear --scale
 done
 
 
-for i in {1..30}
+for i in {1..15}
 do  
     python kernel.py -data OCEA -n_classes 5 -network ResNet --gpu -seed $i -kernel RBF
     python kernel.py -data OCEA -n_classes 5 -network ResNet --gpu -seed $i -kernel IMQ
     python kernel.py -data OCEA -n_classes 5 -network ResNet --gpu -seed $i -kernel Linear
+    python kernel.py -data OCEA -n_classes 5 -network ResNet --gpu -seed $i -kernel RBF --scale
+    python kernel.py -data OCEA -n_classes 5 -network ResNet --gpu -seed $i -kernel IMQ --scale
+    python kernel.py -data OCEA -n_classes 5 -network ResNet --gpu -seed $i -kernel Linear --scale
 done
 
-for i in {1..30}
+for i in {1..15}
 do  
     python kernel.py -data MRI -n_classes 4 -network ResNet --gpu -seed $i -kernel RBF
     python kernel.py -data MRI -n_classes 4 -network ResNet --gpu -seed $i -kernel IMQ
     python kernel.py -data MRI -n_classes 4 -network ResNet --gpu -seed $i -kernel Linear
+    python kernel.py -data MRI -n_classes 4 -network ResNet --gpu -seed $i -kernel RBF --scale
+    python kernel.py -data MRI -n_classes 4 -network ResNet --gpu -seed $i -kernel IMQ --scale
+    python kernel.py -data MRI -n_classes 4 -network ResNet --gpu -seed $i -kernel Linear --scale
 done
